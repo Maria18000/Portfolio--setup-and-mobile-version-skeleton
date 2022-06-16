4dc9b0d548351043c5ceaf726be6e1071130a52c
Maria18000 <maria.hussain18000@gmail.com>
2022-06-16 12:57:57 +0100

    Fixed Linters

--- main.js
+++ main.js
@@ -132,6 +132,18 @@ const openBtn = document.querySelectorAll('[data-modal-target]');
 const closeBtn = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
 openBtn.forEach((button) => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
@@ -152,15 +164,3 @@ closeBtn.forEach((button) => {
     closeModal(modal);
   });
 });
-
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classList.add('active');
-  overlay.classList.add('active');
-}
-
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classList.remove('active');
-  overlay.classList.remove('active');
-}
